fd612e9291d7de0f6399034fdfd279f095ddf3c6
Rob McGuire-Dale <rmcguir@rei.com>
2015-09-09 12:35:57 -0700

    Finish dep collector tests

--- test/hash-spec.js
+++ test/hash-spec.js
@@ -7,22 +7,65 @@ var chai    = require( 'chai' );
 var schai   = require( 'sinon-chai' );
 
 // Configure Chai
-var expect = chai.expect;
+chai.should();
 chai.use( schai );
 
-var getDepTree = function ( overrides ) {
-    return pequire( '../lib/hash', {
+// Configure spies
+var spies = {
+    md5: sinon.stub().returns( 'md5-return' )
+};
 
-    } );
+// Configure module getter
+var getHash = function ( overrides ) {
+    _.forEach( spies, function ( spy ) { spy.reset() } );
+    return pequire( '../lib/hash', _.assign( {}, {
+        md5: spies.md5
+    } ) );
 };
 
 describe( 'hash', function () {
 
     it( 'asserts the root file path exists and is a regular file', function () {
-        getDepTree().bind( null, 'fake-file', {}, function ( err ) {
-            expect( err ).to.equal(
+        getHash().bind( null, 'fake-file', {}, function ( err ) {
+            err.should.equal(
                 'fake-file is not a regular file, or does not exist.'
             )
         } );
+    } );
+
+    it( 'returns a hash of the entry file and its dependency tree' );
+} );
+
+describe( '_depCollector', function () {
+
+    beforeEach( function () {
+        this.pushSpy = sinon.spy();
+        this.nextSpy = sinon.spy();
+        this.hash    = getHash();
+        // Call dep collector with a custom `this` context
+        this.hash._depCollector.bind( { push: this.pushSpy } )(
+            { source: 'fake-dep-source' },
+            null,
+            this.nextSpy
+        );
+    } );
+
+    it( 'hashes every dep source', function () {
+        spies.md5.should.have.been.calledOnce;
+        spies.md5.should.have.been.calledWith( 'fake-dep-source' );
+    } );
+
+    it( 'pushes every hashed dep source onto the hash registry', function () {
+        this.hash._srcHashes.should.deep.equal( [ 'md5-return' ] );
+    } );
+
+    it( 'pushes the dep, unchanged, back onto the stream', function () {
+        this.pushSpy.should.have.been.called.once;
+        this.pushSpy.args[ 0 ][ 0 ]
+            .should.deep.equal( { source: 'fake-dep-source' } );
+    } );
+
+    it( 'calls the `next` callback', function () {
+        this.nextSpy.should.have.been.calledOnce;
     } );
 } );

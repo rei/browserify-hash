deffb1ceb87fc130cd3f42f8b0e9b01b8e39914f
Rob McGuire-Dale <rmcguir@rei.com>
2015-09-09 19:18:47 -0700

    Fill out hash test spec

--- test/hash-spec.js
+++ test/hash-spec.js
@@ -1,23 +1,36 @@
 'use strict';
 
+var path    = require( 'path' );
 var _       = require( 'lodash' );
 var pequire = require( 'proxyquire' );
 var sinon   = require( 'sinon' );
 var chai    = require( 'chai' );
 var schai   = require( 'sinon-chai' );
 
+var MOD_PATH = '../lib/hash.js';
+
 // Configure Chai
 chai.should();
 chai.use( schai );
 
 // Configure spies
-var spies = {
-    lodash_defaults: sinon.stub().returns( {
-        external: [],
-        include:  []
-    } ),
-    md5: sinon.stub().returns( 'md5-return' )
-};
+var spies = {};
+spies.lodash_defaults        = sinon.spy( _.defaults );
+spies.md5                    = sinon.stub().returns( 'md5-return' );
+spies.thru_obj               = sinon.spy();
+spies.bsfy_bundle            = sinon.spy();
+spies.bsfy_pipeline_get_push = sinon.spy();
+spies.bsfy_pipeline_get      = sinon.stub().returns( {
+    push: spies.bsfy_pipeline_get_push
+} );
+spies.bsfy_external = sinon.spy();
+spies.bsfy = sinon.stub().returns( {
+    external: spies.bsfy_external,
+    pipeline: {
+        get: spies.bsfy_pipeline_get
+    },
+    bundle: spies.bsfy_bundle
+} );
 
 // Configure happy-path dependencies
 var HAPPY_DEPS = {
@@ -25,7 +38,7 @@ var HAPPY_DEPS = {
         existsSync: function () {
             return true;
         },
-        lstatSync: function () {
+        lstatSync:  function () {
             return {
                 isFile: function () {
                     return true;
@@ -34,22 +47,23 @@ var HAPPY_DEPS = {
         }
     },
     lodash: _.assign( {}, _, {
-        defaults: spies.lodash_defaults
+        defaults: spies.lodash_defaults,
+        curry:    _.curry
     } ),
-    md5: spies.md5
+    md5: spies.md5,
+    through2: {
+        obj: spies.thru_obj
+    },
+    browserify: spies.bsfy
 };
 
 // Configure module getter
 var getHash = function ( overrides ) {
     _.forEach( spies, function ( spy ) { spy.reset() } );
-    return pequire( '../lib/hash', _.assign( {}, HAPPY_DEPS, overrides ) );
+    return pequire( MOD_PATH, _.assign( {}, HAPPY_DEPS, overrides ) );
 };
 
 describe( 'hash', function () {
-
-    beforeEach( function () {
-        //this.hash = getHash();
-    } );
 
     it( 'asserts the root file path exists and is a regular file', function () {
         var ERR_MSG = 'fake-file is not a regular file, or does not exist.';
@@ -59,7 +73,7 @@ describe( 'hash', function () {
                     return false;
                 }
             } )
-        } )( 'fake-file', {}, function ( err ) {
+        } )( 'fake-file', function ( err ) {
             err.should.equal( ERR_MSG );
         } );
 
@@ -73,28 +87,82 @@ describe( 'hash', function () {
                     };
                 }
             } )
-        } )( 'fake-file', {}, function ( err ) {
+        } )( 'fake-file', function ( err ) {
             err.should.equal( ERR_MSG );
         } );
     } );
 
     it( 'defaults undefined options', function () {
-        getHash()( 'fake-file', { fake: 'option' }, _.noop );
+        getHash()( 'fake-file', _.noop );
         spies.lodash_defaults.should.be.calledOnce;
-        spies.lodash_defaults.args[ 0 ][ 0 ].should.deep.equal( {
-            fake: 'option'
-        } );
         spies.lodash_defaults.args[ 0 ][ 1 ].should.deep.equal( {
-            external: [],
-            include:  []
+            exclude: [],
+            include: []
         } );
     } );
 
-    it( 'creates a new bsfy instance with the entry file' );
+    it( 'creates a new bsfy instance with the entry file', function () {
+        getHash()( 'fake-file', _.noop );
+        spies.bsfy.should.be.calledOnce;
+        spies.bsfy.should.be.calledWith( 'fake-file' );
+    } );
 
-    it( 'pushes the dep collector onto the `deps` pipeline phase' );
+    it( 'tells bsfy to ignore excluded files', function () {
+        getHash()( 'fake-file', _.noop, {
+            exclude: [
+                'fake-dep-a',
+                'fake-dep-b'
+            ]
+        } );
+        spies.bsfy_external.should.be.calledOnce;
+        spies.bsfy_external.args[ 0 ][ 0 ].should.deep.equal( [
+            'fake-dep-a',
+            'fake-dep-b'
+        ] );
+    } );
 
-    it( 'returns a hash of the entry file and its dependency tree' );
+    it( 'pushes the dep collector onto the `deps` pipeline phase', function () {
+        var thruObjSpy = sinon.spy( function ( cb ) {
+            cb().should.equal( 'dep-collector-return' );
+            return 'thru-return';
+        } );
+        var hash = getHash( {
+            through2: {
+                obj: thruObjSpy
+            }
+        } );
+        var depCollectorStub = sinon.stub( hash, '_depCollector' )
+            .returns( 'dep-collector-return' );
+
+        hash()
+
+        thruObjSpy.should.have.been.calledOnce;
+
+        spies.bsfy_pipeline_get_push.should.have.been.calledOnce;
+        spies.bsfy_pipeline_get_push.should.have.been.calledWith( 'thru-return' );
+
+        spies.bsfy_pipeline_get.should.have.been.calledOnce;
+        spies.bsfy_pipeline_get.should.have.been.calledWith( 'deps' );
+    } );
+
+    it( 'calls the bundle handler', function () {
+        var bundleSpy = sinon.spy( function ( cb ) {
+            cb( null, null ).should.equal( 'bundler-return' );
+            return 'bundler-return';
+        } );
+        var hash = getHash( {
+            browserify: function () {
+                return _.assign( {}, spies.bsfy(), {
+                    bundle: bundleSpy
+                } )
+            }
+        } );
+        var bundlerStub = sinon.stub( hash, '_bundler', function ( opts, cb, err, bundle ) {
+            return 'bundler-return';
+        } )
+
+        hash();
+    } );
 } );
 
 describe( '_depCollector', function () {
@@ -105,7 +173,10 @@ describe( '_depCollector', function () {
         this.hash    = getHash();
         // Call dep collector with a custom `this` context
         this.hash._depCollector.bind( { push: this.pushSpy } )(
-            { source: 'fake-dep-source' },
+            {
+                id:     'fake-dep-id',
+                source: 'fake-dep-source'
+            },
             null,
             this.nextSpy
         );
@@ -117,13 +188,18 @@ describe( '_depCollector', function () {
     } );
 
     it( 'pushes every hashed dep source onto the hash registry', function () {
-        this.hash._srcHashes.should.deep.equal( [ 'md5-return' ] );
+        this.hash.srcHashReg.should.deep.equal( {
+            'fake-dep-id': 'md5-return'
+        } );
     } );
 
     it( 'pushes the dep, unchanged, back onto the stream', function () {
         this.pushSpy.should.have.been.called.once;
         this.pushSpy.args[ 0 ][ 0 ]
-            .should.deep.equal( { source: 'fake-dep-source' } );
+            .should.deep.equal( {
+                id:     'fake-dep-id',
+                source: 'fake-dep-source'
+            } );
     } );
 
     it( 'calls the `next` callback', function () {

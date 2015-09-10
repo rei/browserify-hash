52fdd51fc1b399a3930e9f814cf5003ad8784d56
Rob McGuire-Dale <rmcguir@rei.com>
2015-09-09 19:18:32 -0700

    Disabling changed for now until it can be tested

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var _ = require( 'lodash' )
+
 module.exports = {
     hash:    require( './lib/hash' ),
-    changed: require( './lib/changed' )
+    //changed: require( './lib/changed' )
 };
